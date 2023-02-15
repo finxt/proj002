1a66adfc8bfb96caa542c90e8a77580b884acc9c
finxt <finxt@istart-school>
2023-02-16 01:31:44 +0900

    use buit in server

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,24 +1,25 @@
 #!/usr/bin/env bash
 
 echo "00-laravel-deploy.sh"
-php artisan about
 
 # echo "Running composer"
 
 # composer install --no-dev --working-dir=/var/www/html
 
-# echo "JS&CSS build..."
-# npm ci
-# npm run build
+echo "JS&CSS build..."
+npm ci
+npm run build
 
-# echo "Caching config..."
-# php artisan config:cache
+echo "Caching config..."
+php artisan config:cache
 
-# echo "Caching routes..."
-# php artisan route:cache
+echo "Caching routes..."
+php artisan route:cache
 
 echo "Running migrations..."
 php artisan migrate --force
+
+php artisan about
 
 # chown -R sail:sail /var/www/html
 
