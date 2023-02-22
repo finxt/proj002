094254d492d556a75b2fdff12bc808fa0496b2b0
finxt <finxt@istart-school>
2023-02-23 00:25:03 +0900

    in Dockerfile

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -2,25 +2,25 @@
 
 echo "00-laravel-deploy.sh"
 
-echo "Running composer"
-
-composer install --no-dev --working-dir=/var/www/html
-
-echo "JS&CSS build..."
-npm ci
-npm run build
-
-echo "Caching config..."
-php artisan config:cache
-
-echo "Caching routes..."
-php artisan route:cache
-
-echo "Running migrations..."
-php artisan migrate --force
-
-php artisan about
-
-chown -R sail:sail /var/www/html
+# echo "Running composer"
+# 
+# composer install --no-dev --working-dir=/var/www/html
+# 
+# echo "JS&CSS build..."
+# npm ci
+# npm run build
+# 
+# echo "Caching config..."
+# php artisan config:cache
+# 
+# echo "Caching routes..."
+# php artisan route:cache
+# 
+# echo "Running migrations..."
+# php artisan migrate --force
+# 
+# php artisan about
+# 
+# chown -R sail:sail /var/www/html
 
 
