0683087bd5e392503f472468d5df804e56328cb1
finxt <finxt@istart-school>
2023-02-22 23:44:29 +0900

    secret file

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -3,23 +3,23 @@
 echo "00-laravel-deploy.sh"
 php -v
 
-# echo "Running composer"
+echo "Running composer"
 
-# composer install --no-dev --working-dir=/var/www/html
+composer install --no-dev --working-dir=/var/www/html
 
-# echo "JS&CSS build..."
-# npm ci
-# npm run build
+echo "JS&CSS build..."
+npm ci
+npm run build
 
-# echo "Caching config..."
-# php artisan config:cache
+echo "Caching config..."
+php artisan config:cache
 
-# echo "Caching routes..."
-# php artisan route:cache
+echo "Caching routes..."
+php artisan route:cache
 
-# echo "Running migrations..."
-# php artisan migrate --force
+echo "Running migrations..."
+php artisan migrate --force
 
-# chown -R sail:sail /var/www/html
+chown -R sail:sail /var/www/html
 
 
